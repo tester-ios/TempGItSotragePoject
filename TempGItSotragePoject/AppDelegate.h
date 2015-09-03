3db64e2535c7042780e97c77a1232fa83a290a44
Arun <arun@Shubhanks-Mac-mini.local>
2015-09-03 11:52:22 +0530

    Initial Commit

---
+++ TempGItSotragePoject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TempGItSotragePoject
+//
+//  Created by Arun on 9/3/15.
+//  Copyright (c) 2015 Arun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TempGItSotragePoject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TempGItSotragePoject
+//
+//  Created by Arun on 9/3/15.
+//  Copyright (c) 2015 Arun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
